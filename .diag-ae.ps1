9ddef44b8866a7633cbadbbec140826ab29603e2
JUNKDOGE-JOE <2814374544@qq.com>
2026-08-02 00:30:43 +0800

    chore(issue-86): remove temporary T4 diagnostic scripts

--- .diag-ae.ps1
+++
@@ -1,6 +0,0 @@
-$ae = Get-Process AfterFX -ErrorAction SilentlyContinue
-if (-not $ae) { Write-Output 'AE-NOT-RUNNING'; exit }
-Write-Output ('AE-PID: ' + $ae.Id)
-$hit = $ae.Modules | Where-Object { $_.ModuleName -match 'AeMcp' }
-if ($hit) { $hit | ForEach-Object { Write-Output ('MODULE-LOADED: ' + $_.FileName) } }
-else { Write-Output 'MODULE-NOT-LOADED' }
